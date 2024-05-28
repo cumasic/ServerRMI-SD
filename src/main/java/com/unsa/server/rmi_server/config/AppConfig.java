ea4a876d4be4d75d6b5048d637389325e13efebd
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-05-28 00:23:18 -0500

    Fix dependencies

---
+++ src/main/java/com/unsa/server/rmi_server/config/AppConfig.java
@@ -0,0 +1,30 @@
+package com.unsa.server.rmi_server.config;
+
+import com.unsa.server.rmi_server.service.CalculatorService;
+import com.unsa.server.rmi_server.service.CalculatorServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.remoting.rmi.RmiServiceExporter;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+@Configuration
+public class App {
+
+    @Bean
+    public Registry registry() throws RemoteException {
+        return LocateRegistry.createRegistry(1099);
+    }
+
+    @Bean
+    public RmiServiceExporter rmiServiceExporter() throws Exception {
+        RmiServiceExporter exporter = new RmiServiceExporter();
+        exporter.setServiceName("CalculatorServices");
+        exporter.setServiceInterface(CalculatorService.class);
+        exporter.setService(new CalculatorServiceImpl());
+        exporter.setRegistryPort(1099);
+        return exporter;
+    }
+}
